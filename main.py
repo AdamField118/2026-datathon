f677350db5f0db400e0d75ba884935ac9705b691
Joe G <joebgorton@gmail.com>
2026-09-23 18:54:36 -0400

    fixing main (stupid)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 
 if __name__ == "__main__":
-    data_path = "data/basketball-data"
+    data_path = "data/team-stats"
     with open(f"{data_path}/advanced-stats.csv") as adv_file:
         advanced_stats = csv.reader(adv_file)
         for row in advanced_stats:
