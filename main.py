96b60b80c73bdf963463155c8f4006ec55924ad2
Joe G <joebgorton@gmail.com>
2026-09-23 18:52:46 -0400

    fixing data structure

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import csv
+
 if __name__ == "__main__":
-    print("hello world")
+    data_path = "data/basketball-data"
+    with open(f"{data_path}/advanced-stats.csv") as adv_file:
+        advanced_stats = csv.reader(adv_file)
+        for row in advanced_stats:
+            print(row)
